facdc3121c7ac357ff3cc6107e6925fe78f10a81
Delphine <delphine.dara@gmail.com>
2020-04-10 13:04:19 -0500

    Added home filtering section

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,18 +1,12 @@
-import { browser, by, element } from 'protractor';
-/**came with the code when I downloaded it
-export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
-  } **/
 
-  // This is in the code on github
-  export class AppPage {
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
-//---ends here so 
 
-  getTitleText(): Promise<string> {
+  getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
 }
--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homes',
   templateUrl: './homes.component.html'
 })
 export class HomesComponent implements OnInit {
-  
+
+  homeTypeDropdownOpen = false;
+
   homes$ = this.dataService.getHomes();
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  homeTypeFilterApplied($event) {
+
+    this.homeTypeDropdownOpen = false;
+    this.router.navigate(['homes'], { queryParams: { 'home-type': $event } });
+
+  }
+
 }
+
